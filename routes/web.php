8476e36cce5507a2c72bb9690588ce43e39bb3e6
Igor Makhinich <igor.makhinich@gmail.com>
2020-12-18 15:40:04 +0200

    check routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function () {
+    return "About page";
+});
+Route::get('/contact', function () {
+    return "Contact";
+});
+Route::get('/post/{id}/{name}', function($id, $name){
+    return "This is post №" . $id . " " . $name;
+});
+Route::get('admin/posts/example', array('as'=>'admin.home', function(){
+    $url = route('admin.home');
+
+    return "this url is". $url;
+}));
